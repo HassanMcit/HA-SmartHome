ee60da881e7f219e173d03159cc6203715a7622d
HassanMcit <hassan.a173784@gmail.com>
2026-06-13 13:24:19 +0300

    build: optimize compilation configs to prevent memory limit errors on vercel and trigger clean build

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  productionBrowserSourceMaps: false,
   outputFileTracingRoot: path.join(__dirname),
   turbopack: {},
   async rewrites() {
